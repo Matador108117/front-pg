ed5ddf61f07d04ecfb328c11fcf5b9d773d355d1
matador108117 <zS23004741@estudiantes.uv.mx>
2025-04-17 11:34:59 -0600

    add order by

--- src/callrestapi.js
+++ src/callrestapi.js
@@ -1,25 +1,25 @@
 var url = "https://apitrest.onrender.com/api/users";
-
+var allUsers = [];
 function postUser() {
-    var myuser = {
-        name: $('#name').val(),
-        email: $('#email').val(),
-        age: $('#age').val(),
-        comments: $('#comments').val()
-    };
+  var myuser = {
+    name: $('#name').val(),
+    email: $('#email').val(),
+    age: $('#age').val(),
+    comments: $('#comments').val()
+  };
 
-    $.ajax({
-        url: url,
-        type: 'post',
-        dataType: 'json',
-        contentType: 'application/json',
-        data: JSON.stringify(myuser),
-        success: function (data) {
-            Swal.fire('Usuario agregado correctamente', '', 'success');
-            getUsers();
-            closeModal();
-        }
-    });
+  $.ajax({
+    url: url,
+    type: 'post',
+    dataType: 'json',
+    contentType: 'application/json',
+    data: JSON.stringify(myuser),
+    success: function (data) {
+      Swal.fire('Usuario agregado correctamente', '', 'success');
+      getUsers();
+      closeModal();
+    }
+  });
 }
 /*
 function getUsers() {
@@ -54,71 +54,71 @@ function getUsers() {
 }
 */
 async function searchUser() {
-    $('#spinner-container').show();
-    $('#resultado').empty();
-  
-    const input = $('#text').val().trim();
-  
-    if (input === '') {
-      Swal.fire('Empty input ', 'Please type somthing to search', 'warning');
+  $('#spinner-container').show();
+  $('#resultado').empty();
+
+  const input = $('#text').val().trim();
+
+  if (input === '') {
+    Swal.fire('Empty input ', 'Please type somthing to search', 'warning');
+    $('#spinner-container').hide();
+    return;
+  }
+
+  let userData = [];
+
+  if (/^\d+$/.test(input)) {
+    // Búsqueda por ID
+    try {
+      const response = await fetch(`${url}/${input}`);
+      if (!response.ok) throw new Error('User not found');
+
+      const data = await response.json();
+      const user = data.user; // 
+      userData.push(user);
+    } catch (error) {
+      console.error(error);
+      Swal.fire('Not found', 'No user with this ID was found', 'error');
       $('#spinner-container').hide();
       return;
     }
-  
-    let userData = [];
-  
-    if (/^\d+$/.test(input)) {
-      // Búsqueda por ID
-      try {
-        const response = await fetch(`${url}/${input}`);
-        if (!response.ok) throw new Error('User not found');
-        
-        const data = await response.json();     
-        const user = data.user; // 
-        userData.push(user);
-      } catch (error) {
-        console.error(error);
-        Swal.fire('Not found', 'No user with this ID was found', 'error');
+
+  } else {
+    // Búsqueda por nombre
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      const users = data.users;
+
+      userData = users.filter(user =>
+        user.name.toLowerCase().includes(input.toLowerCase())
+      );
+
+      if (userData.length === 0) {
+        Swal.fire('Not found', 'No user with this name was found.', 'info');
         $('#spinner-container').hide();
         return;
       }
-  
-    } else {
-      // Búsqueda por nombre
-      try {
-        const response = await fetch(url);
-        const data = await response.json();
-        const users = data.users;
-  
-        userData = users.filter(user =>
-          user.name.toLowerCase().includes(input.toLowerCase())
-        );
-  
-        if (userData.length === 0) {
-          Swal.fire('Not found', 'No user with this name was found.', 'info');
-          $('#spinner-container').hide();
-          return;
-        }
-  
-      } catch (error) {
-        console.error(error);
-        Swal.fire('Error', 'Oops, Has been a trouble to search users .', 'error');
-        $('#spinner-container').hide();
-        return;
-      }
+
+    } catch (error) {
+      console.error(error);
+      Swal.fire('Error', 'Oops, Has been a trouble to search users .', 'error');
+      $('#spinner-container').hide();
+      return;
     }
-  
-    // Construcción de la tabla
-    let html = `<table>
+  }
+
+  // Construcción de la tabla
+  let html = `<table>
       <thead>
         <tr>
           <th>ID</th><th>Name</th><th>Email</th><th>Age</th><th>Comments</th><th>Actions</th>
         </tr>
       </thead>
       <tbody>`;
-  
-    userData.forEach(user => {
-      html += `<tr>
+
+  userData.forEach(user => {
+    html += `<tr>
         <td>${user.id}</td>
         <td>${user.name}</td>
         <td>${user.email}</td>
@@ -133,54 +133,124 @@ async function searchUser() {
           </button>
         </td>
       </tr>`;
-    });
-  
-    html += `</tbody></table>`;
-  
-    $('#resultado').html(html);
-    $('#spinner-container').hide();
-  }
-  
-  
-  
-function getUser(id){
-  $.getJSON(url + '/' + id, function(json) {
+  });
+
+  html += `</tbody></table>`;
+
+  $('#resultado').html(html);
+  $('#spinner-container').hide();
+}
+
+
+
+function getUser(id) {
+  $.getJSON(url + '/' + id, function (json) {
     let user = json.user;
     $('#name').val(user.name);
     $('#email').val(user.email);
     $('#age').val(user.age);
     $('#comments').val(user.comments);
-    return user; 
+    return user;
   });
 }
-function getUser(id, callback){
-    $.getJSON(url + '/' + id, function(json) {
-      let user = json.user;
-      callback(user);
-    });
-  }
-  
+function getUser(id, callback) {
+  $.getJSON(url + '/' + id, function (json) {
+    let user = json.user;
+    callback(user);
+  });
+}
+
 
 function handleEdit(id) {
-    getUser(id, function(user) {
-        openModal('edit', user);
+  getUser(id, function (user) {
+    openModal('edit', user);
+  });
+}
+function getUsers() {
+  $('#spinner-container').show();
+  $('#resultado').empty();
+
+  $.getJSON(url)
+    .done(function (json) {
+      allUsers = json.users;
+      if (allUsers) {
+        renderUsers(allUsers);
+      }
+    })
+    .fail(function () {
+      $('#spinner-container').hide();
+      $('#resultado').html('<p style="color:red; text-align:center;">Error al cargar usuarios.</p>');
     });
 }
 
-function getUsers() {
-
-    $('#spinner-container').show();
-    $('#resultado').empty(); 
-
-    $.getJSON(url, function (json) {
-        let users = json.users;
-        let html = `<table>
+function renderUsers(allUsers) {
+  let html = `<table>
           <tr>
             <th>ID</th><th>Name</th><th>Email</th><th>Age</th><th>Comments</th><th>Actions</th>
           </tr>`;
 
-        users.forEach(user => {
-            html += `<tr>
+  allUsers.forEach(user => {
+    html += `<tr>
+              <td>${user.id}</td>
+              <td>${user.name}</td>
+              <td>${user.email}</td>
+              <td>${user.age}</td>
+              <td>${user.comments}</td>
+              <td class="accions">
+                <button class="btn-edit" onclick="handleEdit(${user.id})" title="Edit">
+                <i class="bi bi-pencil"></i>
+                </button>
+                <button class="btn-delete" onclick="confirmDelete(${user.id})" title="Delete">
+                <i class="bi bi-trash"></i>
+               </button> 
+              </td>
+            </tr>`;
+  });
+
+  html += '</table>';
+  $('#resultado').html(html);
+  $('#spinner-container').hide();
+}
+
+  
+function sortUsers() {
+  const sortBy = document.getElementById("sortSelect").value;
+
+  const sortedUsers = [...allUsers]; // copia para no afectar el original
+
+  switch (sortBy) {
+    case "id-asc":
+      sortedUsers.sort((a, b) => a.id - b.id);
+      break;
+    case "id-desc":
+      sortedUsers.sort((a, b) => b.id - a.id);
+      break;
+    case "name-asc":
+      sortedUsers.sort((a, b) => a.name.localeCompare(b.name));
+      break;
+    case "name-desc":
+      sortedUsers.sort((a, b) => b.name.localeCompare(a.name));
+      break;
+  }
+
+  renderUsers(sortedUsers);
+}
+/*
+
+function getUsers() {
+
+  $('#spinner-container').show();
+  $('#resultado').empty();
+
+  $.getJSON(url, function (json) {
+    let users = json.users;
+    let html = `<table>
+          <tr>
+            <th>ID</th><th>Name</th><th>Email</th><th>Age</th><th>Comments</th><th>Actions</th>
+          </tr>`;
+
+    users.forEach(user => {
+      html += `<tr>
               <td>${user.id}</td>
               <td>${user.name}</td>
               <td>${user.email}</td>
@@ -197,87 +267,89 @@ function getUsers() {
 
               </td>
             </tr>`;
-        });
-
-        html += '</table>';
-        $('#resultado').html(html);
-
-        $('#spinner-container').hide();
-    }).fail(function () {
-        $('#spinner-container').hide();
-        $('#resultado').html('<p style="color:red; text-align:center;">Error al cargar usuarios.</p>');
     });
+
+    html += '</table>';
+    $('#resultado').html(html);
+
+    $('#spinner-container').hide();
+  }).fail(function () {
+    $('#spinner-container').hide();
+    $('#resultado').html('<p style="color:red; text-align:center;">Error al cargar usuarios.</p>');
+  });
 }
 
+*/
+
 function confirmDelete(id) {
-    Swal.fire({
-        title: 'Are you sure?',
-        text: "this action cannot be undone",
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#F73E7E',
-        cancelButtonColor: '#1A2930',
-        confirmButtonText: 'Yes'
-    }).then((result) => {
-        if (result.isConfirmed) {
-            deleteUser(id);
-        }
-    });
+  Swal.fire({
+    title: 'Are you sure?',
+    text: "this action cannot be undone",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#F73E7E',
+    cancelButtonColor: '#1A2930',
+    confirmButtonText: 'Yes'
+  }).then((result) => {
+    if (result.isConfirmed) {
+      deleteUser(id);
+    }
+  });
 }
 
 function deleteUser(id) {
-    $.ajax({
-        url: url + '/' + id,
-        type: 'delete',
-        success: function () {
-            Swal.fire(' User deleted ', '', 'success');
-            getUsers();
-        }
-    });
+  $.ajax({
+    url: url + '/' + id,
+    type: 'delete',
+    success: function () {
+      Swal.fire(' User deleted ', '', 'success');
+      getUsers();
+    }
+  });
 }
 
 function confirmUpdate(id) {
-    const form = document.getElementById("employeeForm");
+  const form = document.getElementById("employeeForm");
 
-    if (!form.checkValidity()) {
-        form.reportValidity(); 
-        return;
+  if (!form.checkValidity()) {
+    form.reportValidity();
+    return;
+  }
+
+  Swal.fire({
+    title: '¿would you like to update this user?',
+    icon: 'question',
+    showCancelButton: true,
+    confirmButtonColor: '#95df3a',
+    cancelButtonColor: '#ff5c33',
+    confirmButtonText: 'Yes'
+  }).then((result) => {
+    if (result.isConfirmed) {
+      updateUser();
+      closeModal();
     }
-
-    Swal.fire({
-        title: '¿would you like to update this user?',
-        icon: 'question',
-        showCancelButton: true,
-        confirmButtonColor: '#95df3a',
-        cancelButtonColor: '#ff5c33',
-        confirmButtonText: 'Yes'
-    }).then((result) => {
-        if (result.isConfirmed) {
-            updateUser();
-            closeModal();
-        }
-    });
+  });
 }
 
 
 function updateUser() {
-    var id = $('#userId').val();
-    var myuser = {
-        name: $('#name').val(),
-        email: $('#email').val(),
-        age: $('#age').val(),
-        comments: $('#comments').val()
-    };
+  var id = $('#userId').val();
+  var myuser = {
+    name: $('#name').val(),
+    email: $('#email').val(),
+    age: $('#age').val(),
+    comments: $('#comments').val()
+  };
 
-    $.ajax({
-        url: url + '/' + id,
-        type: 'put',
-        dataType: 'json',
-        contentType: 'application/json',
-        data: JSON.stringify(myuser),
-        success: function () {
-            Swal.fire('User updated', '', 'success');
-            getUsers();
-        }
-    });
+  $.ajax({
+    url: url + '/' + id,
+    type: 'put',
+    dataType: 'json',
+    contentType: 'application/json',
+    data: JSON.stringify(myuser),
+    success: function () {
+      Swal.fire('User updated', '', 'success');
+      getUsers();
+    }
+  });
 }
